f9f6f45a31d676ced9c06b915751fafec745bd95
Bufalo <you@example.com>
2022-07-05 11:32:47 +0000

    node

---
+++ test/app.js
@@ -0,0 +1,23 @@
+/*creating a server*/
+const http = require('http');
+const fs = require('fs') /*adding a file*/
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('index.html',(err,html)=>{
+    if(err){
+        throw err;
+    }
+
+
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type','html');
+    res.write(html);
+    res.end();
+});
+
+server.listen(port,hostname,()=>{
+    console.log(`Server started at port ${port}`)
+});
+});
